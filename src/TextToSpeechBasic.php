e607df67e131f8d74d6c14a31b4c0c6db82fd2da
luudoanh <luudoanh26@gmail.com>
2019-06-28 00:06:31 +0700

    Upload the project for the first time.

---
+++ src/TextToSpeechBasic.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App;
+
+/**
+ * This is a class that allows you to convert plain text into audio (mp3) via Google Translate speech (EN)
+ * Đây là một lớp đơn giản giúp bạn có thể chuyển đổi văn bản thành file audio (mp3) thông qua Google Translate (VI)
+ * các bạn thể sử dụng lại class này cho các dự án khác của mình. Mình mong là nó sẽ hửu ích cho bạn.
+ * 
+ */
+class TextToSpeechBasic {
+	/**
+	 * @author 		Luu Doanh
+	 * @copyright 	Luu Doanh (C) 2019
+	 * @link 		https://github.com/luudoanh26/Text-To-Speech-Basic
+	 */
+	
+	/**
+	 * Declare a constant to divide the text
+	 * @var int
+	 */
+	const Number = 200;
+	
+	/**
+	 * raw http header
+	 */
+	const Context = array(
+		'https' => array(
+			'method' => "GET",
+			'header' => "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n" .
+			"accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5\r\n".
+			"user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36\r\n"
+		)
+	);
+
+	/**
+	 * url google translate
+	 * @var string
+	 */
+	const url = 'https://translate.google.com.vn/translate_tts';
+
+	/**
+	 * The directory save audio files when processing is complete
+	 * @var string
+	 */
+	private static $save_folder = '';
+
+	/**
+	 * Array contains punctuation characters.
+	 * @var array
+	 */
+	private $Puntuation = array('.', ',', '?', '!', ':');
+
+	/**
+	 * url with parameters
+	 * @var string
+	 */
+	private $raw_url = '';
+
+	/**
+	 * Array contains uri parameters
+	 * @var array
+	 */
+	
+	private $_config = array(
+		'tl' => 'vi',
+		'ie' => 'UTF-8',
+		'client' => 'tw-ob'
+	);
+
+	/**
+	 * Array contains files list audio
+	 * @var array
+	 */
+	private $fileList = [];
+
+	var $text = '';
+
+	/**
+	 * Set input parameters
+	 * @param  array  $config [description]
+	 * @return [type]         [description]
+	 */
+	public function _setting($config = array()) {
+		foreach ($config as $key => $value) {
+			if (isset($key)) {
+				$this->_config[$key] = $value;
+			}
+		}
+		return $this;
+	}
+
+	/**
+	 * Set url complete
+	 * @param Content text to speech
+	 */
+	private function set_uri($content) {
+		$this->raw_url = self::url.'?ie='.$this->_config['ie'].'&tl='.$this->_config['tl'].'&client='.$this->_config['client'].'&q='.urlencode($content);
+		return $this;
+
+	}
+
+	/**
+	 * Get list file mp3 in save_folder
+	 * @param  Source folder file audio
+	 * @param  Type file (dedault .mp3)
+	 * @return Array contains file list
+	 */
+	private function fromData() {
+		if (!is_dir(self::$save_folder)) {
+			return false;
+		}
+		$this->fileList = [];
+		if ($handle = opendir(self::$save_folder)) {
+			while ($entry = readdir($handle)) {
+				if (preg_match('/part\_[\d]+\.mp3$/i', $entry)) {
+					$this->fileList[] = $entry;
+				}
+			}
+			closedir($handle);
+			natsort($this->fileList);
+			return $this->fileList;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Merge audio files
+	 * @param string $name_full
+	 */
+	private function Merge_Audio($name_full = 'Full_') {
+		if (empty($this->fromData())) {
+			return false;
+		}
+		$this->text = null;
+		foreach ($this->fromData() as $key => $value) {
+			$this->text .= file_get_contents(self::$save_folder.'/'.$value);
+			unlink(self::$save_folder.'/'.$value);
+		}
+		$result = file_put_contents(self::$save_folder.'/'.$name_full.time().'.mp3', $this->text, FILE_APPEND);
+		if ($result) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Save file audios
+	 * @param  integer $part
+	 * @return true
+	 */
+	private function save_file($path,$name='part_', $part=1) {
+		foreach ($this->fromFile($path) as $key => $value) {
+			$url = $this->set_uri($value)->raw_url;
+			$result = copy($url, self::$save_folder.'/'.$name.$part.'.mp3', stream_context_create(self::Context));
+			//sleep(1); //Sometimes we should go to sleep!
+			$part++;
+		}
+		return true;
+	}
+
+	/**
+	 * Get content file text
+	 * @param  path file
+	 * @return array contains words
+	 */
+	private function fromFile($path, $newArray = array(), $data = array()) {
+		if (empty($this->Puntuation)) return false;
+		$str = file_get_contents($path);
+		$str = preg_replace('/\n|\r/', '', $str);
+		while (true) {
+			if (strlen($str) < self::Number) {
+				array_push($newArray, $str);
+				break;
+			}
+			$strs = substr($str, 0, self::Number);
+			$data = array();
+			foreach ($this->Puntuation as $key => $value) {
+				$data[] = abs(strrpos($strs, $value));
+			}
+			$limit = max($data);
+			if ($limit == 0) { //If there is no valid punctuation.
+				$data[] = abs(strrpos($strs, ' ')); //then we use spaces to break sentences.
+				$limit = max($data);
+			}
+			$result = substr($strs, 0, $limit);
+			array_push($newArray, $result);
+			$str = substr($str, ($limit+1), strlen($str));
+		}
+		return $newArray;
+	}
+
+	/**
+	 * Function text to speech
+	 * @param path file text
+	 * @param New folder to save the final file
+	 */
+	public function TTS($path, $save_folder) {
+		if (!is_string($path)) {
+			return false;
+		}
+		if (!is_dir($save_folder)) {
+			mkdir($save_folder, '0755');
+		}
+		self::$save_folder = $save_folder;
+		if (!empty($this->fromFile($path))) {
+			if ($this->save_file($path)) {
+				$result = $this->Merge_Audio();
+				if ($result) {
+					return 'Successful!';
+				} else {
+					return false;
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * Fucntion reset
+	 * @return null
+	 */
+	private function reset() {
+		$this->text = null;
+		$this->fileList = [];
+	}
+
+	/**
+	 * Reset script
+	 */
+	public function __construct() {
+		$this->reset();
+	}
+}
---
+++ tests/TextToSpeechBasicTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\TextToSpeechBasic;
+
+final class TestTextToSpeechBasic extends TestCase {
+	
+	/**
+     * Get a non public property of an object
+     *
+     * @param object $obj
+     * @param string $property
+     * @return mixed
+     */
+	protected function getNonPublicProperty($obj, $property)
+	{
+		if (!is_object($obj) || !is_string($property)) {
+			return null;
+		}
+		$ref = new \ReflectionProperty(get_class($obj), $property);
+		$ref->setAccessible(true);
+
+		return $ref->getValue($obj);
+	}
+
+    /**
+     * Set value for a non public property of an object
+     *
+     * @param object $obj
+     * @param string $property
+     * @param mixed  $value
+     */
+    protected function setNonPublicProperty($obj, $property, $value)
+    {
+    	if (!is_object($obj) || !is_string($property)) {
+    		return;
+    	}
+    	$ref = new \ReflectionProperty(get_class($obj), $property);
+    	$ref->setAccessible(true);
+
+    	$ref->setValue($obj, $value);
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object $obj    Instantiated object that we will run method on.
+     * @param string $method Method name to call
+     * @param array  $params Array of parameters to pass into method.
+     * @return mixed         Method return.
+     * @throws \InvalidArgumentException
+     */
+    protected function invokeNonPublicMethod($obj, $method, $params = [])
+    {
+    	if (!is_object($obj) || !is_string($method)) {
+    		throw new \InvalidArgumentException();
+    	}
+    	$ref = new \ReflectionMethod($obj, $method);
+    	$ref->setAccessible(true);
+
+    	return $ref->invokeArgs($obj, $params);
+    }
+
+	/**
+	 * [testGetFromFileByPathGroupToArray] Function to test the return value of function fromFile
+	 * @param  [array] $desiredArrayReceived [desired array received]
+	 * @param  [string] $filePath             [file path]
+	 * @dataProvider providerTestGetFromFilePathGroupToArray
+	 */
+	public function testGetFromFileByPathGroupToArray($desiredArrayReceived, $filePath) {
+		$tts = new TextToSpeechBasic();
+		$result = $this->invokeNonPublicMethod($tts, 'fromFile', [$filePath]);
+		$resultFinal = $this->arrayAreSmilar($desiredArrayReceived, $result);
+		$this->assertTrue($resultFinal);
+	}
+
+	public function providerTestGetFromFilePathGroupToArray() {
+		$rawArray = Array('Chương 1: Có lẽ gặp nhau đã là một sai lầm?Nhiều năm rồi, mà bầu trời hôm nay vẫn trong xanh như vậy. Nắng xuống đẩy bầu trời lên cao hơn',' Những vệt nắng cuối ngày kéo dài trên mặt đường và dường như bóng tôi cũng kéo dài theo đến vô cùng vô tận. Hoàng hôn buông mình xuống',' phủ đều lên những hàng liễu mảnh mai đang rũ mình khuấy động những con sóng lăn tăn trên mặt hồ',' không rõ là do cảnh chiều tà khiến lòng đột nhiên có chút nhốn nháo, hay là trong tôi vốn đã mang tâm trạng của những ngày cũ','Tôi lặng im dạo bước trên đường Hồ tây Hà Nội, cố thả lỏng mình thư thái để tận hưởng những dư âm cuối ngày',' Buông mình trong một không gian trong lành, tôi hít một hơi thật sâu rồi dừng chân lắng nghe âm thanh của cuộc sống văng vẳng vọng lại lúc xa lúc gần',' Tôi nắm lấy thành lan can mát lạnh, thả mình theo làn gió nhẹ nhàng của ngày đông.Tôi nhìn về phía trước, không biết là nhìn gì',' chỉ biết tôi đang hướng đến một chân trời xa xăm, hay chính là nhìn về những kí ức cũ.Tôi buông mắt xuống hồi tưởng đầy nhớ nhung. Gió thổi nhẹ',' những ngày xưa ấy như một thước phim chầm chậm quay trở về.');
+		$rawPath = 'tests/TextToTest/text.txt';
+		return [
+			[$rawArray, $rawPath]
+		];
+	}
+
+	private function arrayAreSmilar($rawArray, $arrayHasBeenProcessed) {
+		if (count(array_diff_assoc($rawArray, $arrayHasBeenProcessed))) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+}
